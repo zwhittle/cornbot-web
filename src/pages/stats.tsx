3cfd121a962b244411d4ed71873bda0108306a3f
Zach Whittle <zach@zachwhittle.com>
2023-05-23 02:47:50 -0500

    hide placeholder trends

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -48,14 +48,14 @@ export default function StatsWithTrending() {
           className='flex flex-wrap rounded-md items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8'
         >
           <dt className='text-sm font-medium leading-6 text-gray-500'>{stat.name}</dt>
-          <dd
+          {/* <dd
             className={classNames(
               stat.changeType === 'negative' ? 'text-rose-600' : 'text-gray-700',
               'text-xs font-medium'
             )}
           >
             {stat.change}
-          </dd>
+          </dd> */}
           <dd className='w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900'>
             {stat.value}
           </dd>
